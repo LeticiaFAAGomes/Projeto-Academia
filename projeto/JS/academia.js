4a970b5f9c681098f56596e6aabd1d0e581321a4
LeticiaFAAGomes <leticia.gomes@al.infnet.edu.br>
2024-07-13 14:40:35 -0300

    RF01 - Registro de Matrículas

--- projeto/JS/academia.js
+++ projeto/JS/academia.js
@@ -1,26 +1,26 @@
-function validarCamposObrigatorios(oNome, oContato, aIdade, aTaxa, oNivel, oObjetivo, condicao, restricao){
+function validarCamposObrigatorios(oNome, oContato, aIdade, aTaxa, oNivel, oObjetivo, aCondicao, aRestricao) {
     //RN.01 - Validação de Campos Obrigatórios:
     const listaSem = [oNome, oContato, aIdade, aTaxa, oNivel, oObjetivo];
-    const listaCom = [oNome, oContato, aIdade, aTaxa, oNivel, oObjetivo, restricao];
-    return decisao = condicao == "sim"? listaCom.every(item => item !== "") : listaSem.every(item => item !== "");
+    const listaCom = [oNome, oContato, aIdade, aTaxa, oNivel, oObjetivo, aRestricao];
+    return decisao = aCondicao === "sim"? listaCom.every(item => item !== "") : listaSem.every(item => item !== "");  
 }
 
-function calcularNivel(oNivel){
+function calcularNivel(oNivel) {
     //RN.02 - Validação de Nível:
     if ( oNivel > 0) {
         if (oNivel == 0) {
-            return "Iniciante Absoluto";
+            return "iniciante Absoluto";
         } else if(oNivel < 12) {
-            return "Iniciante";
+            return "iniciante";
         } else if (oNivel < 48) {
             return "intermediario";
         } else {
-            return "Avançado";
+            return "avançado";
         }
     }
 }
 
-function validarNomeCompleto(oNomeCompleto){
+function validarNomeCompleto(oNomeCompleto) {
     //RN.03 - Validação de Nome Completo do Aluno:
     if (oNomeCompleto.includes(" ")){
         parte = oNomeCompleto.split(" ");
@@ -41,40 +41,39 @@ function validarNumeros(oContato) {
     return oContato.value.length > 11 ? oContato.value : false;    
 }
 
-function mostrarDetalhes(aCondicao) {
-    //RN.05 - Validação de Detalhes:
-    restricao = document.getElementById("restricao");
-    aCondicao == "sim" ? restricao.style.display = "inline" : restricao.style.display = "none"; 
+function mostrarDetalhes(aCondicao, aRestricao) {
+    // RN.05 - Validação de Detalhes:
+    aCondicao === "sim" ? aRestricao.style.display = "inline" : aRestricao.style.display = "none"; 
 }
 
-function calcularValorTotal(oPlano, aTaxa){
+function calcularValorTotal(oPlano, aTaxa) {
     //RN.06 - Validação da Matrícula:
     return aTaxa > 0 ? (oPlano + aTaxa).toFixed(2) : false;
 }
 
-function calcularData(){
+function calcularData() {
     //RN.07 - Registro de Data da Matrícula:
     return new Date().toLocaleString();
 }
 
-function gerarNumeroMatricula(){
+function gerarNumeroMatricula() {
     //RN.08 - Geração de Número de Transação Único:
     return Math.floor(Math.random() * 10000);
 }
 
-function registrarHistorico(aMensagem){
-    //RN.09 - Histórico de Alterações:
-    const ulHistoricoAlteracao = document.getElementById("historicoAlteracao");
-    const liHistorico = document.createElement("li");
-    liHistorico.textContent = aMensagem
-    ulHistoricoAlteracao.appendChild(liHistorico);
-} 
+function registrarHistorico(aMensagem, oBotao) {
+    // RN.09 - Histórico de Alterações:
+            const ulHistoricoAlteracao = document.getElementById("historicoAlteracao");
+            const liHistorico = document.createElement("li");
+            liHistorico.textContent = aMensagem;
+            ulHistoricoAlteracao.appendChild(liHistorico);
+}
 
 function registrarMatricula(nomeCompleto, contato, idade, taxa, planoMensalidade, nivel,  condicao, restricao, objetivo){
     
     mostrarDetalhes(condicao, restricao);
     let numeros = validarNumeros(contato);
-    let camposObrigatorios = validarCamposObrigatorios(nomeCompleto, contato.value, idade, taxa, nivel, objetivo, condicao, restricao);
+    let camposObrigatorios = validarCamposObrigatorios(nomeCompleto, contato.value, idade, taxa, nivel, objetivo, condicao, restricao.value);
     let nome = validarNomeCompleto(nomeCompleto)
     let niveis = calcularNivel(nivel);
     let total = calcularValorTotal(planoMensalidade, taxa);
@@ -105,18 +104,19 @@ function registrarMatricula(nomeCompleto, contato, idade, taxa, planoMensalidade
 }
 
 function matricular() {
-    let nomeCompleto = document.getElementById("nome").value;
-    let contato = document.getElementById("contato");
-    let idade = parseInt(document.getElementById("idade").value);
-    let taxa = parseFloat(document.getElementById("taxa").value);
-    let nivel = parseInt(document.getElementById("nivel").value);
-    let planoMensalidade = parseFloat(document.getElementById("planos").value);
-    let restricao = document.getElementById("restricao").value;
-    let condicao = document.getElementById("condicao").value;
-    let objetivo = document.getElementById("objetivo").value;
+    const nomeCompleto = document.getElementById("nome").value;
+    const contato = document.getElementById("contato");
+    const idade = parseInt(document.getElementById("idade").value);
+    const taxa = parseFloat(document.getElementById("taxa").value);
+    const nivel = parseInt(document.getElementById("nivel").value);
+    const planoMensalidade = parseFloat(document.getElementById("planos").value);
+    const restricao = document.getElementById("restricao");
+    const condicao = document.getElementById("condicao").value;
+    const objetivo = document.getElementById("objetivo").value;
+    const botao = document.getElementById("btn");
     let mensagem = registrarMatricula(nomeCompleto, contato, idade, taxa, planoMensalidade, nivel,  condicao, restricao, objetivo);
 
-    registrarHistorico(mensagem)
+    registrarHistorico(mensagem, botao)
 }
 
-matricular()
+validarNumeros()
